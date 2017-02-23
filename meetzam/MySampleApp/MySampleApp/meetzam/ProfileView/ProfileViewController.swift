657481f359f7dd33dbd2216d8e994b1b739ffa9f
ZuYuan Fan <fanzuyuan1996@gmail.com>
2017-02-22 20:12:31 -0500

    Bug fix.

---
+++ meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  MySampleApp
+//
+//  Created by ZuYuan Fan on 2/21/17.
+//
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
