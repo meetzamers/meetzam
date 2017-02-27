490177685fb48e40bd2d46aef551a7fbeaaf58c1
ZuYuan Fan <fanzuyuan1996@gmail.com>
2017-02-26 22:19:51 -0500

    Move log out button to setting page

--- meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/SettingViewController.swift
+++ meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/SettingViewController.swift
@@ -1,36 +1,38 @@
 //
-//  TabBarHomeViewController.swift
+//  SettingViewController.swift
 //  MySampleApp
 //
-//  Created by ZuYuan Fan on 2/20/17.
+//  Created by ZuYuan Fan on 2/26/17.
 //
 //
 
 import UIKit
 import AWSMobileHubHelper
 
-class TabBarHomeViewController: UIViewController {
-    
+class SettingViewController: UIViewController {
+
     // ============================================
     // Variable starts here
     var new_signinObserver: AnyObject!
     var new_signoutObserver: AnyObject!
     
+    fileprivate let loginStatusButton: UIButton = UIButton(frame: CGRect(x: 0, y: UIScreen.main.bounds.height - 50, width: UIScreen.main.bounds.width, height: 50))
+    
     // Variable ends here
     // ============================================
-    // Change status bar type to default.
-    override func viewDidAppear(_ animated: Bool) {
-        UIApplication.shared.statusBarStyle = .default
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        handleSigninStatus()
+        
         
         // ============================================
         // AWS implementation starts here
         
         // 1. first attempt to pop sign in view controller
         perform(#selector(popSignInViewController), with: nil, afterDelay: 0)
+        //popSignInViewController()
         
         // 2. signinObserver: need to figure it out.
         new_signinObserver = NotificationCenter.default.addObserver(
@@ -40,9 +42,9 @@ class TabBarHomeViewController: UIViewController {
             using: { [weak self] (note: Notification) -> Void in
                 guard let strongSelf = self else { return }
                 print("Sign in observer observed sign in.")
-                //strongSelf.setloginStatusButton()
+                strongSelf.setloginStatusButton()
                 
-            })
+        })
         
         // 3. signoutObserver: need to figure it out.
         new_signoutObserver = NotificationCenter.default.addObserver(
@@ -52,33 +54,86 @@ class TabBarHomeViewController: UIViewController {
             using: { [weak self] (note: Notification) -> Void in
                 guard let strongSelf = self else { return }
                 print("Sign Out Observer observed sign out.")
-                //strongSelf.setloginStatusButton()
+                strongSelf.setloginStatusButton()
         })
+        
+        // 4. attemp to add button
+        self.setloginStatusButton()
         
         // AWS implementation ends here
         // ============================================
         
     }
-    
+
     // ============================================
     // AWS support functions start here
+    
     deinit {
         NotificationCenter.default.removeObserver(new_signinObserver)
         NotificationCenter.default.removeObserver(new_signoutObserver)
     }
     
+    // 1. set login/logout button
+    func setloginStatusButton() {
+        
+        loginStatusButton.setTitle("NONE", for: .normal)
+        loginStatusButton.backgroundColor = UIColor.brown;
+        loginStatusButton.tag = 1
+        
+        
+        if (AWSIdentityManager.default().isLoggedIn) {
+            loginStatusButton.setTitle("Log out", for: .normal)
+            loginStatusButton.addTarget(self, action: #selector(handleLogout), for: .touchUpInside)
+            
+        }
+        
+        self.view.addSubview(loginStatusButton)
+    }
     
-    // display sign in view controller
+    // 2. handle logout
+    func handleLogout() {
+        if (AWSIdentityManager.default().isLoggedIn) {
+            AWSIdentityManager.default().logout(
+                completionHandler: { (result: Any?, error: Error?) in
+                    self.setloginStatusButton()
+                    //self.popSignInViewController()
+                    self.animated_SignInViewController()
+            })
+        }
+        else {
+            assert(false)
+        }
+    }
+    
+    // 3. display sign in view controller
     func popSignInViewController() {
         if (!AWSIdentityManager.default().isLoggedIn) {
             let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
             let viewController = storyboard.instantiateViewController(withIdentifier: "SignIn")
-
+            
             self.present(viewController, animated: false, completion: nil)
-
+            
         }
     }
-    // AWS support functions end here
+    
+    func animated_SignInViewController() {
+        if (!AWSIdentityManager.default().isLoggedIn) {
+            let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "SignIn")
+            
+            self.present(viewController, animated: true, completion: nil)
+            
+        }
+    }
+    
     // ============================================
+    func handleSigninStatus() {
+        if (!AWSIdentityManager.default().isLoggedIn) {
+            let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "SignIn")
+            self.present(viewController, animated: true, completion: nil)
+        }
+    }
+
 
 }
