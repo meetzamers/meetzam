64fac3e52a71f9c63200051ac08c819092c2d78b
ZuYuan Fan <fanzuyuan1996@gmail.com>
2017-02-27 18:09:40 -0500

    Some changes in Profile view and Edit Profile view

--- meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/ProfileViewController.swift
+++ meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/ProfileViewController.swift
@@ -18,7 +18,6 @@ class ProfileViewController: UIViewController {
  
     @IBOutlet weak var firstMovie: UIImageView!
     @IBOutlet weak var secondMovie: UIImageView!
-
     @IBOutlet weak var thirdMovie: UIImageView!
     
     override func viewDidLoad() {
@@ -26,15 +25,11 @@ class ProfileViewController: UIViewController {
         
         let identityManager = AWSIdentityManager.default()
         
-        AWSIdentityManager.default()
-        
+        //AWSIdentityManager.default()
         
         if let identityUserName = identityManager.userName {
-            //if let identityAge = identityManager.
-            
             displayNameAndAgeField.text = identityUserName
             
-            //displayNameAndAgeField.text = identityUserName
         } else {
             displayNameAndAgeField.text = NSLocalizedString("Guest User", comment: "Placeholder text for the guest user.")
         }
@@ -47,17 +42,31 @@ class ProfileViewController: UIViewController {
             let imageData = try! Data(contentsOf: imageURL)
             if let profileImage = UIImage(data: imageData) {
                 userPicField.image = profileImage
-                firstMovie.image = profileImage
-                secondMovie.image = profileImage
-                thirdMovie.image = profileImage
+                
+                firstMovie.image = UIImage(named: "lala")
+                firstMovie.contentMode = .scaleAspectFill   // Ryan: Let the image aspect fit the UIImageView size
+        
+                secondMovie.image = UIImage(named: "John")
+                secondMovie.contentMode = .scaleAspectFill  // Ryan: Let the image aspect fit the UIImageView size
+                
+                thirdMovie.image = UIImage(named: "loganposter2")
+                thirdMovie.contentMode = .scaleAspectFill   // Ryan: Let the image aspect fit the UIImageView size
+                
+                
             } else {
                 userPicField.image = UIImage(named: "UserIcon")
             }
         }
-        
-        
-
-        
+    }
+    
+    // Ryan: Edit button clicked action
+    @IBAction func toEditProfilePage(_ sender: Any) {
+        self.performSegue(withIdentifier: "toEditProfileSegue", sender: self)
+    }
+    
+    // Ryan: More button clicked action
+    @IBAction func moreButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "toDetailedMovies", sender: self)
     }
     
     // Go to setting page
