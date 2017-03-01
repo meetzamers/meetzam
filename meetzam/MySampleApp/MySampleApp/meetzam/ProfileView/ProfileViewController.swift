0e6e71738278ca907e93495ea82198eba2a82c6d
孟琦 <monika@monika.local>
2017-03-01 11:26:55 -0500

    changed liked movies to collection view

--- meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/ProfileViewController.swift
+++ meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/ProfileViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Foundation
 import AWSMobileHubHelper
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var displayNameAndAgeField: UILabel!
     @IBOutlet weak var userBioField: UILabel!
@@ -21,10 +21,10 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //get user name from facebook
         let identityManager = AWSIdentityManager.default()
         
         AWSIdentityManager.default()
-        
         
         if let identityUserName = identityManager.userName {
             //if let identityAge = identityManager.
@@ -36,10 +36,11 @@ class ProfileViewController: UIViewController {
             displayNameAndAgeField.text = NSLocalizedString("Guest User", comment: "Placeholder text for the guest user.")
         }
         
+        //mannually typed bio
         userBioField.text = "Hello! :)"
         
         
-        
+        //get user profile picture from facebook
         if let imageURL = identityManager.imageURL {
             let imageData = try! Data(contentsOf: imageURL)
             if let profileImage = UIImage(data: imageData) {
@@ -49,6 +50,9 @@ class ProfileViewController: UIViewController {
             }
         }
   
+        //show top three movies
+        TopThreeMovieCollectionView.delegate = self;
+        TopThreeMovieCollectionView.dataSource = self;
 
         
     }
@@ -62,5 +66,26 @@ class ProfileViewController: UIViewController {
     @IBAction func toSettingButton(_ sender: Any) {
         self.performSegue(withIdentifier: "toSetSegue", sender: self)
     }
+    
+    //setting up top three movie collection view
+    //conform with UICollectionView protocal
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return topThreeImages.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = TopThreeMovieCollectionView.dequeueReusableCell(withReuseIdentifier: "topThreeCell", for: indexPath) as! TopThreeMovieCell
+        
+        cell.Top3MovieImage.image = UIImage(named: topThreeImages[indexPath.row])
+        return cell
+    }
+    
+    
+    var topThreeImages = ["split","loganposter2","lala"]
+    
+    
+    @IBOutlet weak var TopThreeMovieCollectionView: UICollectionView!
+    
+    
 
 }
