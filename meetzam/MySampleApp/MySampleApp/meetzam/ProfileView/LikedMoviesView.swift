40ca190b5cf9773feb9ad473fea3bca5afbafe8d
孟琦 <meng46@purdue.edu>
2017-03-28 01:38:25 -0400

    update

--- meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/LikedMoviesView.swift
+++ meetzam/MySampleApp/MySampleApp/meetzam/ProfileView/LikedMoviesView.swift
@@ -14,46 +14,56 @@ class LikedMoviesView: UIViewController, UICollectionViewDelegate, UICollectionV
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        movieCollectionView.reloadData()
+        
         self.view.backgroundColor = UIColor.init(red: 233/255, green: 233/255, blue: 233/255, alpha: 1)
         
         movieCollectionView.delegate = self
         movieCollectionView.dataSource = self
         
-        var imagesURLs = SingleMovie().getLikedMoviePosters(key: AWSIdentityManager.default().identityId!)
-        /*
+                /*
         print("     put into imagesURLs")
         print("-------------------------------------------------")
         for url in imagesURLs {
             print("This is url \(url)")
         }
         */
+        /*
+        var imagesURLs = SingleMovie().getLikedMoviePosters(key: AWSIdentityManager.default().identityId!)
+
         let count = imagesURLs.count;
         
+        imageData = [Data]()
         imageData.removeAll()
         
         for var i in (0..<count) {
             let path = "https://image.tmdb.org/t/p/w500" + imagesURLs[i]
             let pathURL = URL(string: path)
-            imageData.append(try! Data(contentsOf: pathURL!))
+            imageData.insert(try! Data(contentsOf: pathURL!),at:0)
         }
+         */
 
     }
     
+    
     override func viewWillAppear(_ animated: Bool) {
-        var imagesURLs = SingleMovie().getLikedMoviePosters(key: AWSIdentityManager.default().identityId!)
+       /* var imagesURLs = SingleMovie().getLikedMoviePosters(key: AWSIdentityManager.default().identityId!)
         
         let count = imagesURLs.count;
+        imageData = [Data]()
         imageData.removeAll()
         
         for var i in (0..<count) {
             let path = "https://image.tmdb.org/t/p/w500" + imagesURLs[i]
             let pathURL = URL(string: path)
-            imageData.append(try! Data(contentsOf: pathURL!))
+            //imageData.append(try! Data(contentsOf: pathURL!))
+            imageData.insert(try! Data(contentsOf: pathURL!),at:0)
         }
-        
+        */
         movieCollectionView.reloadData()
 
     }
+ 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -70,10 +80,11 @@ class LikedMoviesView: UIViewController, UICollectionViewDelegate, UICollectionV
     
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = movieCollectionView.dequeueReusableCell( withReuseIdentifier: "CustomCell", for: indexPath) as! MovieCollectionCell
         
+        var imageData = updateMovieImages()
         
-        //cell.movieImage.image = UIImage(named: images[indexPath.row])
         cell.movieImage.image = UIImage(data: imageData[indexPath.row])
         
         cell.movieTitleLabel.text = "hello"
@@ -83,10 +94,25 @@ class LikedMoviesView: UIViewController, UICollectionViewDelegate, UICollectionV
         return cell
     }
     
+    public func updateMovieImages() -> [Data] {
+        let imagesURLs = SingleMovie().getLikedMoviePosters(key: AWSIdentityManager.default().identityId!)
+        
+        //let count = imagesURLs.count;
+        var imageData = [Data]()
+        //imageData.removeAll()
+        
+        for i in (0..<imagesURLs.count) {
+            let path = "https://image.tmdb.org/t/p/w500" + imagesURLs[i]
+            let pathURL = URL(string: path)
+            //imageData.append(try! Data(contentsOf: pathURL!))
+            imageData.insert(try! Data(contentsOf: pathURL!),at:0)
+        }
+        
+        return imageData
+    }
+    
     @IBOutlet weak var movieCollectionView: UICollectionView!
-    
-    var imageData = [Data]()
-    
+   
 }
 
 
